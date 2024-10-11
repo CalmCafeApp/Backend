9851883107ea61827676f971490ac9a8026ef2f4
ParkSangsin <pss6039@naver.com>
2024-10-11 18:36:53 +0900

    feature: add logic to retrieve cafe congestion

--- src/main/java/kau/CalmCafe/store/controller/StoreController.java
+++ src/main/java/kau/CalmCafe/store/controller/StoreController.java
@@ -5,10 +5,12 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import kau.CalmCafe.Congestion.domain.CongestionLevel;
 import kau.CalmCafe.global.api_payload.ApiResponse;
 import kau.CalmCafe.global.api_payload.SuccessCode;
 import kau.CalmCafe.store.converter.StoreConverter;
 import kau.CalmCafe.store.domain.Store;
+import kau.CalmCafe.store.dto.StoreResponseDto.StoreCongestionFromUserDto;
 import kau.CalmCafe.store.dto.StoreResponseDto.StoreDetailFromCafeDto;
 import kau.CalmCafe.store.dto.StoreResponseDto.StoreDetailResDto;
 import kau.CalmCafe.store.service.StoreService;
@@ -30,9 +32,9 @@ public class StoreController {
     private final StoreService storeService;
     private final UserService userService;
 
-    @Operation(summary = "유저 측 매장 상세 정보 조회", description = "유저 측에서 매장의 상세 정보를 조회하는 메서드입니다.")
+    @Operation(summary = "유저 측 매장 상세 정보 조회", description = "유저 측 화면에서 매장의 상세 정보를 조회하는 메서드입니다.")
     @ApiResponses(value = {
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "STORE_2001", description = "유저 측 매장 상세 정보 조회가 완료되었습니다.")
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "STORE_2001", description = "유저 측 화면에서 매장 상세 정보 조회가 완료되었습니다.")
     })
     @Parameters({
             @Parameter(name = "address", description = "매장 주소"),
@@ -49,12 +51,12 @@ public class StoreController {
 
         Integer distance = storeService.calDistance(userLatitude, userLongitude, store.getLatitude(), store.getLongitude());
 
-        return ApiResponse.onSuccess(SuccessCode.STORE_DETAIL_SUCCESS, StoreConverter.storeDetailResDto(store, distance));
+        return ApiResponse.onSuccess(SuccessCode.STORE_DETAIL_FROM_USER_SUCCESS, StoreConverter.storeDetailResDto(store, distance));
     }
 
-    @Operation(summary = "카페 측 매장 상세 정보 조회", description = "카페 측에서 상세 정보를 조회하는 메서드입니다.")
+    @Operation(summary = "카페 측 매장 상세 정보 조회", description = "카페 측 화면에서 상세 정보를 조회하는 메서드입니다.")
     @ApiResponses(value = {
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "STORE_2002", description = "카페 측 매장 상세 정보 조회가 완료되었습니다.")
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "STORE_2002", description = "카페 측 화면에서 매장 상세 정보 조회가 완료되었습니다.")
     })
     @GetMapping("/detail/cafe")
     public ApiResponse<StoreDetailFromCafeDto> getStoreDetailFromCafe(
@@ -64,7 +66,23 @@ public class StoreController {
 
         Store store = user.getStore();
 
-        return ApiResponse.onSuccess(SuccessCode.STORE_DETAIL_SUCCESS, StoreConverter.storeDetailFromCafeDto(store));
+        return ApiResponse.onSuccess(SuccessCode.STORE_DETAIL_FROM_CAFE_SUCCESS, StoreConverter.storeDetailFromCafeDto(store));
+    }
+
+    @Operation(summary = "유저 측 매장 혼잡도 조회", description = "유저 측 화면에서 특정 매장의 혼잡도를 조회하는 메서드입니다.")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "STORE_2005", description = "유저 측 화면에서 매장 혼잡도 조회가 완료되었습니다.")
+    })
+    @Parameters({
+            @Parameter(name = "storeId", description = "매장 id"),
+    })
+    @GetMapping("/detail/user/congestion")
+    public ApiResponse<StoreCongestionFromUserDto> getStoreCongestionFromUser(
+            @RequestParam(name = "storeId") Long storeId
+    ) {
+        Store store = storeService.findById(storeId);
+
+        return ApiResponse.onSuccess(SuccessCode.STORE_CONGESTION_GET_SUCCESS, StoreConverter.storeCongestionFromUserDto(store));
     }
 
 }
