24e9d29d523965c50939d4298a76e731fe7ac164
JGBean <gyubin0712@naver.com>
2024-10-12 17:02:05 +0900

    feature: modify store details

--- src/main/java/kau/CalmCafe/store/controller/StoreController.java
+++ src/main/java/kau/CalmCafe/store/controller/StoreController.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import kau.CalmCafe.Congestion.domain.CongestionLevel;
 import kau.CalmCafe.global.api_payload.ApiResponse;
 import kau.CalmCafe.global.api_payload.ErrorCode;
 import kau.CalmCafe.global.api_payload.SuccessCode;
@@ -89,9 +88,26 @@ public class StoreController {
 
         return ApiResponse.onSuccess(SuccessCode.STORE_CONGESTION_GET_SUCCESS, StoreConverter.storeCongestionFromUserDto(store));
     }
+
+    @Operation(summary = "사용자 화면 주소를 통한 주변 매장 좌표 조회", description = "사용자 화면 주소를 통해 주변 매장 좌표를 조회합니다.")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "STORE_2009", description = "주변 매장 좌표 조회가 완료되었습니다.")
+    })
+    @Parameters({
+            @Parameter(name = "userAddress", description = "사용자 화면의 주소 정보")
+    })
+    @GetMapping("/")
+    public ApiResponse<StorePosListDto> getNearStorePosList(
+            @RequestParam(name = "userAddress") String userAddress
+    ) {
+        List<Store> storeList = storeService.getNearStoreList(userAddress);
+
+        return ApiResponse.onSuccess(SuccessCode.STORE_NEAR_LIST_SUCCESS, StoreConverter.storePosListDto(storeList));
+    }
+
     @Operation(summary = "매장 영업 시간 수정", description = "사장님이 매장의 영업 시간을 수정하는 메서드입니다.")
     @ApiResponses(value = {
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "STORE_2003", description = "매장 영업 시간 수정이 완료되었습니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "STORE_2006", description = "매장 영업 시간 수정이 완료되었습니다."),
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "STORE_4001", description = "권한이 없습니다.")
     })
     @PatchMapping("/modify/hours")
@@ -112,7 +128,7 @@ public class StoreController {
 
     @Operation(summary = "마지막 주문 시간 수정", description = "사장님이 매장의 마지막 주문 시간을 수정하는 메서드입니다.")
     @ApiResponses(value = {
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "STORE_2005", description = "매장 영업 시간 수정이 완료되었습니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "STORE_2007", description = "매장 영업 시간 수정이 완료되었습니다."),
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "STORE_4001", description = "권한이 없습니다.")
     })
     @PatchMapping("/modify/lastordertime")
@@ -121,22 +137,6 @@ public class StoreController {
             @RequestParam String lastOrderTime
     ) {
         User user = userService.findByUserName(customUserDetails.getUsername());
-
-    @Operation(summary = "사용자 화면 주소를 통한 주변 매장 좌표 조회", description = "사용자 화면 주소를 통해 주변 매장 좌표를 조회합니다.")
-    @ApiResponses(value = {
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "STORE_2006", description = "주변 매장 좌표 조회가 완료되었습니다.")
-    })
-    @Parameters({
-            @Parameter(name = "userAddress", description = "사용자 화면의 주소 정보")
-    })
-    @GetMapping("/")
-    public ApiResponse<StorePosListDto> getNearStorePosList(
-            @RequestParam(name = "userAddress") String userAddress
-    ) {
-        List<Store> storeList = storeService.getNearStoreList(userAddress);
-
-        return ApiResponse.onSuccess(SuccessCode.STORE_NEAR_LIST_SUCCESS, StoreConverter.storePosListDto(storeList));
-    }
 
         if (user.getRole() != Role.CAFE) {
             return ApiResponse.onFailure("STORE_4002", "권한이 없습니다.", null);
@@ -148,7 +148,7 @@ public class StoreController {
 
     @Operation(summary = "매장 최대 수용 인원 수정", description = "사장님이 매장의 최대 수용 인원을 수정하는 메서드입니다.")
     @ApiResponses(value = {
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "STORE_2004", description = "매장 최대 수용 인원 수정이 완료되었습니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "STORE_2008", description = "매장 최대 수용 인원 수정이 완료되었습니다."),
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "STORE_4002", description = "권한이 없습니다.")
     })
     @PatchMapping("/modify/max-capacity")
@@ -166,3 +166,4 @@ public class StoreController {
         return ApiResponse.onSuccess(SuccessCode.STORE_CAPACITY_UPDATE_SUCCESS, StoreConverter.storeDetailFromCafeDto(updatedStore));
     }
 }
+
